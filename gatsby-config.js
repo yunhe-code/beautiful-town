c344bc4783fedfe4008e31d66399169b001667f6
XiYiZhou <55862635+xiyizhou@users.noreply.github.com>
2020-09-09 19:07:00 +0800

    美丽城镇服务平台

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: '美丽城镇服务平台',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      '美丽城镇建设服务平台',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
