e7caf28f30986343a083401306c551998663d952
XiYiZhou <55862635+xiyizhou@users.noreply.github.com>
2020-09-09 18:51:35 +0800

    更新 继续阅读

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -47,7 +47,7 @@ class BlogRoll extends React.Component {
                   <br />
                   <br />
                   <Link className="button" to={post.fields.slug}>
-                    Keep Reading →
+                    继续阅读 →
                   </Link>
                 </p>
               </article>
