177e0381b292d5573f473b44001dcc35ad4e5092
XiYiZhou <55862635+xiyizhou@users.noreply.github.com>
2020-09-10 08:56:10 +0800

    update color

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -36,7 +36,7 @@ const TemplateWrapper = ({ children }) => {
         <link
           rel="mask-icon"
           href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
-          color="#ff4400"
+          color="#17B496"
         />
         <meta name="theme-color" content="#fff" />
 
