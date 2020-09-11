a65b871e4bde71d9385ae9141b22e685984d363c
XiYiZhou <55862635+xiyizhou@users.noreply.github.com>
2020-09-11 13:58:39 +0800

    up 资讯

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -13,17 +13,6 @@ export default class BlogIndexPage extends React.Component {
             backgroundImage: `url('/img/blog-index.jpg')`,
           }}
         >
-          <h1
-            className="has-text-weight-bold is-size-1"
-            style={{
-              boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-              backgroundColor: '#17B496',
-              color: 'Black',
-              padding: '1rem',
-            }}
-          >
-            最新资讯
-          </h1>
         </div>
         <section className="section">
           <div className="container">
